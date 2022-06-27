f5b26eb6e0877925bf2faa72f6ac67e7faf567f9
Anastasia Ryzhova <aruzhova@yandex.ru>
2022-06-28 01:23:35 +0300

    fixed settings

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -16,7 +16,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-secret-key')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(' ')
 
